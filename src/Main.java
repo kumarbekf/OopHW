6c43c6f72cdcc450792f4e0bae557785dc6a583c
kumarbek <kumarbektolonbayev@gmail.com>
2023-07-09 19:46:59 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        Triangle j = new Triangle();
+        j.a = 6;
+        j.b = 7;
+        j.c = 4;
+        j.area();
+    }
+}
